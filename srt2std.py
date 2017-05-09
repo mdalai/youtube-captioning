77f07156ef60206e5c4cf7e47ce658920ae93542
swidc <swidc@cuw.edu>
2017-05-09 12:05:20 -0500

    update srt2std

--- srt2std.py
+++ srt2std.py
@@ -31,6 +31,12 @@ def get_next_target(text):
     
     end_time = text[end_time_pos:end_time_pos+12]
 
+    # check if we need to process
+    line2_start_time2 = text[find_nth_index(text,"\n",end_time_pos,5): find_nth_index(text,"\n",end_time_pos,5)+12]
+    line2_start_time = text[text.find("--> ",end_time_pos)-13:text.find("--> ",end_time_pos)-1]
+    if line2_start_time2 == line2_start_time:
+        return None, None
+
 
     line3_start_time_pos = text.find("--> ",text.find("--> ",end_time_pos)+4)
     if line3_start_time_pos == -1:
@@ -42,32 +48,26 @@ def get_next_target(text):
     if datetime.strptime(end_time, '%H:%M:%S,%f') < datetime.strptime(line3_start_time, '%H:%M:%S,%f'):
         end_time = line3_start_time
     
-    #end_line_pos = text.find("\n",end_time_pos)+1
     line1_start_pos = text.find("\n",end_time_pos)+1
-    #end_line_pos1 = find_nth_index(text,"\n",end_line_pos,1)
     line1_end_pos = find_nth_index(text,"\n",line1_start_pos,1)
-    #end_line_pos2 = find_nth_index(text,"\n",line1_start_pos,4)
     line2_start_pos = find_nth_index(text,"\n",line1_start_pos,4)
-    #end_line_pos3 = find_nth_index(text,"\n",line1_start_pos,5)+1
     line2_end_pos = find_nth_index(text,"\n",line1_start_pos,5)+1
     
     txt2line = text[2:end_time_pos] + end_time +'\n' + text[line1_start_pos:line1_end_pos] + text[line2_start_pos:line2_end_pos]
     end_pos = line3_start_time_pos-15
 
-    # Later add: If line3_end_time == line2_end time: add 3rd line to the above 2 lines as well.
+    # Later add: If line3_end_time == line2_end time: merge 3rd line into the above 2 lines as well.
     line3_end_time = text[line3_start_time_pos+4:line3_start_time_pos+16]
     line2_end_time = text[line2_start_pos-13:line2_start_pos-1]
     if datetime.strptime(line2_end_time, '%H:%M:%S,%f') == datetime.strptime(line3_end_time, '%H:%M:%S,%f'):
+        end_time = line3_end_time
         line3_start_pos = find_nth_index(text,"\n",line3_start_time_pos,1)
         line3_end_pos = find_nth_index(text,"\n",line3_start_time_pos,2)+1
         txt2line = text[2:end_time_pos] + end_time +'\n' + text[line1_start_pos:line1_end_pos] + text[line2_start_pos:line2_end_pos-1] + text[line3_start_pos:line3_end_pos]
         end_pos =  find_nth_index(text,"\n",line3_end_pos,1)-2
         return txt2line, end_pos
 
-    # check if we need to process
-    line2_start_time = text[text.find("--> ",end_time_pos)-13:text.find("--> ",end_time_pos)-1]
-    if datetime.strptime(end_time, '%H:%M:%S,%f') <= datetime.strptime(line2_start_time, '%H:%M:%S,%f'):
-        return None, None
+    
 
     return txt2line, end_pos
     
